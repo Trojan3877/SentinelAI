4470c0afd60ec72ab82f2f00caac8d3b2d9dfa29
Corey Leath <corey22blue@hotmail.com>
2025-12-26 04:35:34 -0500

    Create Navbar.tsx

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="border-b border-gray-800 p-4 text-white">
+      <span className="font-bold">SentinelAI</span>
+    </nav>
+  );
+}
